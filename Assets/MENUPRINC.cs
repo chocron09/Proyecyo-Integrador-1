49c348500e0a96cc969a378700fe5600de017f43
chocron09 <juandavidvalencia@MacBook-Pro-de-JUAN.local>
2015-11-10 22:42:08 -0500

    sm-57

---
+++ Assets/MENUPRINC.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MENUPRINC : MonoBehaviour {
+public void POLMIC(string level)
+    {
+        Application.LoadLevel("POLMIC");
+    }
+
+      public void FREQMIC(string level)
+    {
+        Application.LoadLevel("FREQMIC");
+    }
+
+    public void POLARBACK(string level)
+    {
+        Application.LoadLevel("MIC");
+    }
+    public void FREQBACK(string level)
+    {
+        Application.LoadLevel("MIC");
+    }
+}
